07342e92aa3458929b7227d57ae4e3ce447b3fb7
Matt Neary <neary.matt@gmail.com>
2013-10-23 21:48:31 -0400

    server and client page

--- schedule.rb
+++ schedule.rb
@@ -1,3 +1,5 @@
+require 'sinatra'
+
 class Block
   attr_accessor :length, :flexible
   def initialize(length, flexible)
@@ -32,15 +34,32 @@ def get_period(num)
   { 0 => 0, 1 => 2, 2 => 4, 3 => 6, 4 => 8, 5 => 11, 6 => 13, 7 => 15, 8 => 18 }[num]
 end
 def fit_periods(a, b, lunch, finish, start=Time.now)
-  fit_upto(periods(lunch)[get_period(a)..get_period(b)], start, finish)
+  slice = periods(lunch)[get_period(a)..get_period(b)]
+  p slice.map(&:length)
+  fit_upto(slice, start, finish)
 end
-def render_periods(ps)
-  start = Time.new(2013, 10, 23, 7, 55, 0).to_i
-  stamps = ps.map(&:length).reduce([[0,0]]) {|a,x| a.concat([[a.last[0] + a.last[1], x]])}.select {|x|
+def render_periods(ps, start)
+  start = start.to_i
+  stamps = ps.map(&:length).reduce([[0,0]]) {|a,x|
+    a.concat([[a.last[0] + a.last[1], x]])
+  }.select {|x|
     x[1] != 5
   }[1..-1].map {|x| 
-    Time.at(x.first*60+start).strftime("%H:%M:%S") + " - " + Time.at((x.first+x.last)*60+start).strftime("%H:%M:%S")
-  }
+    Time.at(x.first*60 + start).strftime("%H:%M:%S") + " - " + Time.at((x.first + x.last)*60 + start).strftime("%H:%M:%S")
+  }.join "<br>"
 end
 
-puts render_periods(fit_periods(0, 8, 77, Time.new(2013, 10, 23, 15, 13, 0), Time.new(2013, 10, 23, 7, 55, 0)))
+get '/api/:start/:end/:from/:to/:lunch' do
+  from = params[:from].split(":").map(&:to_i)
+  to = params[:to].split(":").map(&:to_i)
+  start = params[:start].to_i
+  endd = params[:end].to_i
+  lunch = params[:lunch].to_i
+  startTime = Time.new(2013, 10, 23, from.first, from.last, 0)
+  endTime = Time.new(2013, 10, 23, to.first, to.last, 0)
+  render_periods(fit_periods(start, endd, lunch, endTime, startTime), startTime)
+end
+
+get '/' do
+  File.read('index.html')
+end
